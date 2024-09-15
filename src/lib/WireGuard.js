d2a1c13bedb929c2055174c8b00f52d496550ccd
Jelenkee <59470612+Jelenkee@users.noreply.github.com>
2024-09-15 15:30:39 +0200

    wg0 dns

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -105,6 +105,7 @@ module.exports = class WireGuard {
 PrivateKey = ${config.server.privateKey}
 Address = ${config.server.address}/24
 ListenPort = ${WG_PORT}
+${WG_DEFAULT_DNS ? `DNS = ${WG_DEFAULT_DNS}` : ''}
 PreUp = ${WG_PRE_UP}
 PostUp = ${WG_POST_UP}
 PreDown = ${WG_PRE_DOWN}
